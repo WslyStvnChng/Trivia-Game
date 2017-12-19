335d77dc4a0b01ab5c5b9c243186fba66f47b52c
Steven^C <mr.cheng.st@gmail.com^C>
2017-12-19 02:33:53 -0700

    Created global variables, need to add questions in an object to be able to grab questions

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+  //Creating global variables to initize the game
+  var start = 30;
+
+  var correctAnswer = 0;
+
+  var wrongAnswer = 0;
+
+  var trivia = [
+    {
+      question: "Question 1: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 2: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 3: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 4: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 5: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 6: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 7: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 8: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 9: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    },
+    {
+      question: "Question 10: Who killed Nora Allen in The Flash Comics?",
+      answers: ["Eobard Thawne", "Henry Allen", "Captain Cold", "Hunter Zolomon"],
+      correct: "Eobard Thawne"
+    
+}
+]
