b9ed77d30a51c04461127d4e26bcb8deeefabf70
WslyStvnChng <mr.cheng.st@gmail.com^C>
2017-12-26 20:39:32 -0700

    new thought of changing the questions back to objects with an array inside of it. That way it won't be confusing with individuals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,159 +1,95 @@
 
-const myTrivia = [
-  {
+var number = 10;
+var intervalId;
+var myListofQuestions = [
+{
   question: "Question 1: Who is the All-Time Scoring leader in the NBA?",
-  answers: {
-    a: "Kobe Bryant",
-    b: "Michael Jordan",
-    c: "Karl Malone",
-    d: "Kareem Abdul-Jabbar"
-  },
+  choices: ["Kobe Bryant", "Michael Jordan", "Karl Malone", "Kareem Abdul-Jabbar"],
   correctAnswer: "d"
 },
 {
   question: "Question 2: Hakeem Olajuwon has the most blocks ever in NBA history, how many blocks does he have?",
-    answers: {
-      a: "3,820",
-      b: "3,800",
-      c: "3,810",
-      d: "3,825"
-    },
-    correctAnswer: "a"
+  choice: ["3,820", "3,800", "3,810", "3,825"],
+  correctAnswer: "a"
 },
 {
   question: "Question 3: What team won the very first NBA game?",
-    answers: {
-      a: "Tornoto Huskies",
-      b: "Chicago Stags",
-      c: "New York Knicks",
-      d: "Denver Nuggets"
-    },
-    correctAnswer: "c"
+  choices: ["Tornoto Huskies", "Chicago Stags", "New York Knicks","Denver Nuggets"],
+  correctAnswer: "c"
 },
 {
   question: "Question 4: Shaquille O'Neal was known to have one of the worst FT% of NBA history until which player took his rank?",
-    answers: {
-      a: "Wilt Chamberlain",
-      b: "Ben Wallace",
-      c: "Andre Drummond",
-      d: "Deandre Jordan"
-    },
-    correctAnswer: "c"
-  },
-  {
+  choices: ["Wilt Chamberlain", "Ben Wallace", "Andre Drummond","Deandre Jordan"],
+  correctAnswer: "c"
+},
+{
   question: "Question 5: What year was the National Basketball Association first established?",
-    answers: {
-      a: "1958",
-      b: "1946",
-      c: "1943",
-      d: "1940",
-  },
-    correctAnswer: "b"
-  },
-  {
+  choices: ["1958", "1946", "1943","1940"],
+  correctAnswer: "b"
+},
+{
   question: "Question 6: Michael Jordan was drafted third overall in 1984. Which two players were selected ahead of him?",
-    answers: {
-      a: "Patrick Ewing & Hakeem Olajuwon",
-      b: "Hakeem Olajuwon & Sam Bowie",
-      c: "Karl Malone & Sam Bowie",
-      d: "Charles Barkley & Hakeem Olajuwon"
-    },
-    correctAnswer: "b"
-  },
-  {
+  choices: ["Patrick Ewing & Hakeem Olajuwon", "Hakeem Olajuwon & Sam Bowie", "Karl Malone & Sam Bowie", "Charles Barkley & Hakeem Olajuwon"],
+  correctAnswer: "b"
+},
+{
   question: "Question 7: When was the last time Denver Nuggets won the NBA Championships?",
-    answers: {
-      a: "1960",
-      b: "1965",
-      c: "1962",
-      d: "none"
-    },
-    correctAnswer: "d"
-  },
-  {
+  choices: ["1960", "1965", "1962", "none"],
+  correctAnswer: "d"
+},
+{
   question: "Question 8: Who retired July 16, 2016 before Kobe Bryant and Tim Duncan?",
-    answers: {
-      a: "Shawn Marion",
-      b: "Elton Brand",
-      c: "Amar'e Stoudemire",
-      d: "Kenyon Martin"
-    },
-    correctAnswer: "c"
-  },
-  {
+  choices: ["Shawn Marion", "Elton Brand", "Amar'e Stoudemire", "Kenyon Martin"],
+  correctAnswer: "c"
+},
+{
   question: "Question 9: At the end of 2015-2016 NBA season. Who led the league in total rebounds?",
-    answers: {
-      a: "Andre Drummond",
-      b: "Anthony Davis",
-      c: "Deandre Jodan",
-      d: "Hassan Whiteside"
-    },
-    correctAnswer: "a"
-  },
-  {
+  choices: ["Andre Drummond", "Anthony Davis", "Deandre Jodan", "Hassan Whiteside"],
+  correctAnswer: "a"
+},
+{
   question: "Question 10: Which of these NBA taems did not have a total of four All-Stars on their during a certain year?",
-    answers: {
-      a: "13-14' Atlanta Hawks",
-      b: "16-17' Golden State Warriors",
-      c: "82-83' Philadelphia 76ers",
-      d: "10-11 Boston Celtics"
-    },
-    correctAnswer: "d"
+  choices: ["13-14' Atlanta Hawks", "16-17' Golden State Warriors", "82-83' Philadelphia 76ers", "10-11 Boston Celtics"],
+  correctAnswer: "d"
   }
 ];
 
-function startTimer(duration, display) {
-  var timer = duration,
-    minutes, seconds;
-  setInterval(function () {
-    minutes = parseInt(timer / 60, 10)
-    seconds = parseInt(timer % 60, 10);
+function startGame() {
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+  $("#startGameButton").toggleClass("hidden");
+  $("#gameWrapper").toggleClass("hidden");
 
-    display.textContent = minutes + ":" + seconds;
+  myQuestions = "";
 
-    if (--timer < 0) {
-      timer = duration;
-      stop();
-    }
-  }, 1000);
+  myListofQuestions.forEach(function(obj,idx) {
+
+      myListofQuestions = myQuestion + "<p class='bold'>" + obj.question +"<p>";
+
+      obj.choices.forEach(function(choice) {
+        myQuestion = myQuestion + "<input type='radio' name=" + idx + " value" + choice + ">" + choice + "<br>";
+
+        console.log(myQuestion);
+      })
+
+  })
+  $("#myForm").append(myQuestion);
+  run();
 }
 
-window.onload = function () {
-  var fiveMinutes = 60 * 8,
-    display = document.querySelector('.timer');
-  startTimer(fiveMinutes, display);
-};
+function gradeMe() { 
 
-var triviaContainer = $('#question');
-var results = $('#results');
-var playBtn = $('#btn');
+  stop();
 
-(function() {
-function startTrivia () {
-  var output = [];
+  var numVeryCorrectAnswer = 0;
+  var unanswered = 0;
+  var incorrect = 0;
 
-  trivia.forEach((currentQuestion, questionNumber) => {
-    //
-    var answers = [];
+  //show choice
 
-    for(letter in currentQuestion.answers) {
-      //adding an HTML radio button
-      answers.push(
-        `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-      );
-      }
-       // add this question and its answers to the output
-       output.push(
-       `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-       );
-       return();
-    });
+  for (var i = 0; i < myListofQuestions.length; i++) {
+    var isItChecked = $("input[name =" + i + "]:checked").val();
+
+    if (isItChecked === myListofQuestions[i].correctAnswer);
+  }
+}
 
