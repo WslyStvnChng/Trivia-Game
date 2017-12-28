b3fca67b14aeac2cf6a5aa1dbab3d486bfcd4f10
WslyStvnChng <mr.cheng.st@gmail.com^C>
2017-12-28 02:38:51 -0700

    Trying to adjust css for start button. And changing the div container color to implement change when trivia starts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,35 @@
+//creating a platform for all the questions
 var panel = $("#quiz-area");
 var countStartNumber = 20;
+var myMusic;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-//CLICK EVENTS
+var aud = document.getElementById("myAudio");
+aud.volume = 0.2; // 1 means 100%
+//Making a function with buttons in Js
 
 ///////////////////////////////////////////////////////////////////////////////
 
+//A button to restart the game when game ends
 $(document).on("click", "#start-over", function(e) {
   game.reset();
 });
 
+//When buttons clicked, a function will happen
 $(document).on("click", ".answer-button", function(e) {
   game.clicked(e);
 });
 
+//Start button will create a function to start the game,
+//Grabbing the selector of the container, push it onto the DOM while creatiting a new tag <h2>...</h2?
 $(document).on("click", "#start", function(e) {
   $("#subwrapper").prepend(
     '<h2>Time Remaining: <span id="counter-number">20</span> Seconds</h2>'
   );
+  //recall the function later
   game.loadQuestion();
 });
-
 ///////////////////////////////////////////////////////////////////////////////
 
 //Question set
@@ -32,14 +40,14 @@ var questions = [
   {
     question: "How many total number of points scored by The Truth?",
     answers: ["26,946", "26,397", "26,397", "26,395"],
-    correctAnswer: "26,397"
-    // image: "assets/images/toystory.gif"
+    correctAnswer: "26,397",
+    image: "assets/images/pierce.jpg"
   },
   {
     question: "Which coach won the most during his first three seasons?",
     answers: ["Greg Popovich", "Brad Stevens", "Steve Kerr", "Rick Carlisle"],
-    correctAnswer: "Steve Kerr"
-    // image: "assets/images/spicegirls.gif"
+    correctAnswer: "Steve Kerr",
+    image: "assets/images/kerr.jpeg"
   },
   {
     question: "Which NBA Hall of Fame blocked 3,830 shots in his career?",
@@ -48,39 +56,44 @@ var questions = [
       "Dikembe Mutombo",
       "Hakeem Olajuwon",
       "Mark Eaton"],
-    correctAnswer: "Hakeem Olajuwon"
-    // image: "assets/images/bulls.gif"
+    correctAnswer: "Hakeem Olajuwon",
+    image: "assets/images/hakeem.jpg"
   },
   {
     question: "What player has the most assist in one game?",
     answers: ["Steve Nash", "Kevin Porter", "Bob Cousey", "Scott Skiles"],
-    correctAnswer: "Scott Skiles"
-    // image: "assets/images/nirvanabark.gif"
+    correctAnswer: "Scott Skiles",
+    image: "assets/images/skiles.jpg"
   },
   {
     question: "Who's the most paid NBA player in 2017?",
     answers: ["The King", "Brodi", "One of the Lob City", "Chef Curry"],
-    correctAnswer: "Chef Curry aka Stephen Curry"
-    // image: "assets/images/lionking.gif"
+    correctAnswer: "Chef Curry aka Stephen Curry",
+    image: "assets/images/curry.jpg"
   },
   {
     question: "Who was not part of 'The Big Three'?",
     answers: ["L-Train aka The King", "Flash aka D-Wade", "James Worthy","CB4 aka The Boshtrich"
     ],
-    correctAnswer: "James Worthy"
-    // image: "assets/images/fresh.gif"
+    correctAnswer: "James Worthy",
+    image: "assets/images/worthy.jpg"
   },
   {
     question: "Who is 24 th on the all time nba scoring list?",
     answers: ["Vince Carter", "Allen Iverson", "Ray Alen", "Alex English"],
     correctAnswer: "Allen Iverson",
-    // image: "assets/images/skeeter.gif"
+    image: "assets/images/iverson.jpg"
   },
   {
     question: "2016-2017 Golden State Warriors NBA Finals against Cleveland Cavaliers was eventful! How many points did Lebron James and Kevin Durant score in game 3?",
     answers: ["39 and 31", "31 and 39", "41 and 39", "39 and 41"],
     correctAnswer: "39 and 31",
-    // image: "assets/images/belding.gif"
+    image: "assets/images/gamethree.jpg"
+  },
+  {
+    question: "Nam this song in the background?",
+    answers: ["Love", "Humble", "Loyality", "DNA"],
+    correctAnswer: "Humble",
   }
 ];
 
@@ -104,6 +117,7 @@ var game = {
     panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
     for (var i = 0; i < questions[this.currentQuestion].answers.length; i++) {
       panel.append(
+        //each button is registered to each answer
         '<button class="answer-button" id="button"' +
           'data-name="' +
           questions[this.currentQuestion].answers[i] +
@@ -111,7 +125,9 @@ var game = {
           questions[this.currentQuestion].answers[i] +
           "</button>"
       );
-      // document.getElementById(loadQuestion).style.color = "#ff0000";
+      document.getElementById("subwrapper").style.background="#4aaaa5";
+      document.getElementById("subwrapper").style.height = "200px";
+
     }
   },
   nextQuestion: function() {
